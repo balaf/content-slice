9d12a55887227c3c6d2337034ee53d4fd6334cc6
Elias Balafoutis <ebalafoutis@worky.local>
2015-12-01 18:53:16 +0200

    tag recursion unfinished

--- mssql/index.js
+++ mssql/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var mssql = require('./db');
+//var mssql = require('./db');
 
 var query = new Map();
 
@@ -19,7 +19,9 @@ var DOI_CONTENT_MEMBERS = "select ContentID, doi from DOI_CONTENT_MEMBERS";
 var SLIDING_ISSUE_CONTENT = "select  ContentID, Area, Type, IssueTypes, PubID, CategoryID, Direction, Volumes, Issues from SLIDING_ISSUE_CONTENT";
 var SLIDING_RANGE_CONTENT = "select ContentID, Area, Type, IssueTypes, PubID, Direction, Years, Months, Days from SLIDING_RANGE_CONTENT";
 var ISSUE_TYPE_CONTENT = "select ContentID, Area, Type, PubID, IssueTypeID from ISSUE_TYPE_CONTENT";
-//var TAG_CONTENT = "select ContentID, Area, Type, IssueTypes, TagID from TAG_CONTENT";
+var NUMBER_OF_ISSUES_CONTENT = "select ContentID, Area, Type, IssueTypes, PubID, StartDate, startVol, startIssue, NumberOfIssues from NUMBER_OF_ISSUES_CONTENT";
+var TAG_CONTENT = "select ContentID, Area, Type, IssueTypes, TagID from TAG_CONTENT";
+var TAG_PARENT = "select tagid from TAG_PARENT where parentID=";
 
 var LICENSE = "select top 10 ContentID, userID, offerID, licenseID from LICENSE";
 var GENERIC_OFFER = "select ContentID, offerID from GENERIC_OFFER";
@@ -27,6 +29,7 @@ var DEPOSIT_FEED = "select ContentID, DepositFeedID from DEPOSIT_FEED where Cont
 var DATASET = "select ContentID, DatasetID from DATASET where ContentID is not NULL";
 
 query.set("GENERIC_CONTENT", GENERIC_CONTENT);
+
 query.set("ARTICLE_TYPE_CONTENT",ARTICLE_TYPE_CONTENT);
 query.set("URI_CONTENT", URI_CONTENT);
 query.set("CONTENT_FORMAT_CONTENT", CONTENT_FORMAT_CONTENT);
@@ -37,6 +40,11 @@ query.set("DOI_CONTENT_MEMBERS", DOI_CONTENT_MEMBERS);
 query.set("SLIDING_ISSUE_CONTENT", SLIDING_ISSUE_CONTENT);
 query.set("SLIDING_RANGE_CONTENT", SLIDING_RANGE_CONTENT);
 query.set("ISSUE_TYPE_CONTENT", ISSUE_TYPE_CONTENT);
+query.set("NUMBER_OF_ISSUES_CONTENT", NUMBER_OF_ISSUES_CONTENT);
+query.set("TAG_CONTENT", TAG_CONTENT);
+query.set("TAG_PARENT", TAG_PARENT);
+
+
 query.set("LICENSE", LICENSE);
 query.set("GENERIC_OFFER", GENERIC_OFFER);
 query.set("DEPOSIT_FEED", DEPOSIT_FEED);
@@ -51,3 +59,58 @@ module.exports.getContent = function(param,callback) {
     callback(err,results);
   });
 };
+
+// 
+// function getTagChildren (parent, callback) {
+//   var finalResult = [];
+//
+//   var findChidrenCbk = function (parent, function(err,results){
+//     finalResult.push(parent.tagid);
+//     for (let i=0;i<results.length;i++) {
+//       findChildren(results[i], findChildrenCbk)
+//     }
+//   };
+//   //mssql.query("select tagid from TAG_PARENT where parentid="+parent, function (err,results){
+//   findChildren(parent,findChidrenCbk)
+// }
+
+
+var start = {
+  'c' : { tagid:3, children: []},
+  'd' : { tagid:4, children: []},
+  'g' : { tagid:7, children: []},
+  'f' : { tagid:6, children: []},
+  'e' : { tagid:5, children: ['f']},
+  'b' : { tagid:2, children: ['c','d']},
+  'a' : { tagid:1, children: ['b','e']},
+  root : {tagid:0, children: ['a','g']}
+};
+
+function findChildren(parent, callback) {
+  setTimeout(function(){
+    console.log("Children of "+parent.tagid+" are "+ parent.children);
+    var results = [];
+    console.log("Number of Children:", parent.children.length);
+    for (let i=0;i<parent.children.length;i++) {
+      console.log("Pushing Child["+ i+ "]:", parent.children[i]);
+      results.push(start[parent.children[i]]);
+    }
+    callback(null,results)
+  }, 1000)
+};
+
+
+function findChildrenPlain(parent) {
+  var results = [];
+  for (let i=0;i<parent.children.length;i++) {
+    results.push(findChildrenPlain(start[parent.children[i]]));
+  }
+  return results;
+}
+
+console.log(findChildrenPlain(start['b']));
+
+//
+// getTagChildren(start['b'], function(err,result) {
+//   console.log("-------------Final result:", result);
+// });
